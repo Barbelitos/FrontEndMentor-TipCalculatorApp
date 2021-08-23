76e349f9031deccc1e7ad0e84dcc1e7d22fa53aa
David Fonseca <davidfonseca@davids-mbp.lan>
2021-08-23 19:44:47 +0300

    Percentage buttons selection logic added.

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,24 @@
+// Input values
 const bill = document.querySelector(".bill");
-const percentageButton = document.querySelector(".percentage-button");
+const percentageButton = document.querySelectorAll(".percentage-button");
+const customPercentage = document.querySelector(".percentage-field");
+const people = document.querySelector(".people");
+
+//Results
+const tipPerson = document.querySelector(".tip-person");
+const totalPerson = document.querySelector(".total-person");
+
+percentageButton.forEach((percentage) => {
+  percentage.addEventListener("click", () => {
+    if (percentage.classList.contains("selected")) {
+      percentage.classList.remove("selected");
+      percentage.classList.add("unselected");
+    } else {
+      percentageButton.forEach((e) => {
+        e.classList.remove("selected");
+      });
+      percentage.classList.remove("unselected");
+      percentage.classList.add("selected");
+    }
+  });
+});
