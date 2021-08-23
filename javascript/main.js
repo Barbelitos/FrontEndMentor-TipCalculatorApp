9d385b74911078b3d1e7fd2ad42132c05a9226e1
David Fonseca <davidfonseca@davids-mbp.lan>
2021-08-23 22:22:06 +0300

    Added JS for percentage values selection.

--- javascript/main.js
+++ javascript/main.js
@@ -8,17 +8,34 @@ const people = document.querySelector(".people");
 const tipPerson = document.querySelector(".tip-person");
 const totalPerson = document.querySelector(".total-person");
 
+let tipPercentage = 0;
+let mealAmount = 0;
+let peopleNumber = 1;
+
 percentageButton.forEach((percentage) => {
   percentage.addEventListener("click", () => {
     if (percentage.classList.contains("selected")) {
+      tipPercentage = 0;
       percentage.classList.remove("selected");
       percentage.classList.add("unselected");
     } else {
-      percentageButton.forEach((e) => {
-        e.classList.remove("selected");
+      percentageButton.forEach((event) => {
+        event.classList.remove("selected");
       });
+      tipPercentage = percentage.value; // is giving UNDEFINED -- WHY?
       percentage.classList.remove("unselected");
       percentage.classList.add("selected");
+      console.log(tipPercentage);
     }
+    customPercentage.value = "";
   });
+});
+
+customPercentage.addEventListener("input", () => {
+  if (customPercentage.value >= 0) {
+    percentageButton.forEach((event) => {
+      event.classList.remove("selected");
+    });
+    tipPercentage = customPercentage.value;
+  }
 });
